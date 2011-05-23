9e80f58b405591af93bc9bfe9b1d71fb6a4c39a0
Ray Bejjani <ray.bejjani@gmail.com>
2011-05-22 22:05:02 -0700

    Separated functionality between processing a file i/o

--- resgen.py
+++ resgen.py
@@ -14,8 +14,10 @@ footdict = {'1206_resistor.fp':'1206',
 
 
 # -------------------------- End configuration -----------------------
+
+
+# -------------------------- File IO & Shell -------------------------
 import os
-import math
 import shutil
 
 """ getconf()
@@ -32,6 +34,51 @@ def getconf():
         fin.close()
 
     return rawfile
+
+def makehorz(resdict):
+    """ makehorz(Resistor dictionary)
+    Make the horizontal symbol """
+    outname = resdict['name'] + '_horz.sym'
+    if os.access(outname,os.F_OK):
+        print ('resgen.makehorz: ' + outname + ' already exists.')
+        return
+    else:
+        print('resgen.makehorz: creating ' + outname)
+        fot = open(outname,'w')
+        fhead = open(hfile)
+        try:
+            fot.write(processhorz(resdict, fhead.read()))
+        finally:
+            fhead.close()
+            fot.close()
+
+def makevert(resdict):
+    """ makevert(Resistor dictionary)
+    Make the vertical symbol """
+    outname = resdict['name'] + '_vert.sym'
+    if os.access(outname,os.F_OK):
+        print ('resgen.makevert: ' + outname + ' already exists.')
+        return
+    else:
+        print('resgen.makevert: creating ' + outname)
+        fot = open(outname,'w')
+        fhead = open(hfile)
+        try:
+            fot.write(processvert(resdict, fhead.read()))
+        finally:
+            fhead.close()
+            fot.close()
+
+""" main() """
+def main():
+    resdict = processconf(getconf())
+    resdict['name'] = makename(resdict)
+    makehorz(resdict)
+    makevert(resdict)
+
+
+# -------------------------- Pure Python -----------------------------
+import math
 
 def processconf(rawfile):
     resdict = {}
@@ -126,57 +173,29 @@ def makevalue(resdict):
             valstr = valstr[0:-1] # Reduce to specified precision
     return valstr
 
-
-""" makehorz(Resistor dictionary)
-    Make the horizontal symbol """
-def makehorz(resdict):
-    outname = resdict['name'] + '_horz.sym'
-    if os.access(outname,os.F_OK):
-        print ('resgen.makehorz: ' + outname + ' already exists.')
-        return
-    else:
-        print('resgen.makehorz: creating ' + outname)
-        fot = open(outname,'w')
-        fot.close() # Just touch the file -- don't leave it open
-        shutil.copyfile(hfile,outname)
-        fot = open(outname,'a') # Open for appending
-        fot.write('T 0 1300 8 10 0 0 0 0 1' + '\n') # Footprint
-        fot.write('footprint=' + resdict['footprint'] + '\n')
-        fot.write('T 0 1095 8 10 0 0 0 0 1' + '\n') # Part number
-        fot.write('part=' + resdict['part'] + '\n')
-        fot.write('T 1300 0 8 10 1 1 0 0 1' + '\n') # Value
-        fot.write('value=' + makevalue(resdict) + '\n')
-        fot.close()
-
-""" makevert(Resistor dictionary)
-    Make the vertical symbol """
-def makevert(resdict):
-    outname = resdict['name'] + '_vert.sym'
-    if os.access(outname,os.F_OK):
-        print ('resgen.makevert: ' + outname + ' already exists.')
-        return
-    else:
-        print('resgen.makevert: creating ' + outname)
-        fot = open(outname,'w')
-        fot.close() # Just touch the file -- don't leave it open
-        shutil.copyfile(vfile,outname)
-        fot = open(outname,'a') # Open for appending
-        fot.write('T 0 1700 8 10 0 0 0 0 1' + '\n') # Footprint
-        fot.write('footprint=' + resdict['footprint'] + '\n')
-        fot.write('T 0 1495 8 10 0 0 0 0 1' + '\n') # Part number
-        fot.write('part=' + resdict['part'] + '\n')
-        fot.write('T 300 500 8 10 1 1 0 0 1' + '\n') # Value
-        fot.write('value=' + makevalue(resdict) + '\n')
-        fot.close()
+def processhorz(resdict, template):
+    """Construct a horizonal part"""
+    return "".join([template,
+                    'T 0 1300 8 10 0 0 0 0 1' + '\n', # Footprint
+                    'footprint=' + resdict['footprint'] + '\n',
+                    'T 0 1095 8 10 0 0 0 0 1' + '\n', # Part number
+                    'part=' + resdict['part'] + '\n',
+                    'T 1300 0 8 10 1 1 0 0 1' + '\n', # Value
+                    'value=' + makevalue(resdict) + '\n',
+                ])
 
 
+def processvert(resdict, template):
+    """Construct a vertical part"""
+    return "".join(['T 0 1700 8 10 0 0 0 0 1' + '\n', # Footprint
+                    'footprint=' + resdict['footprint'] + '\n',
+                    'T 0 1495 8 10 0 0 0 0 1' + '\n', # Part number
+                    'part=' + resdict['part'] + '\n',
+                    'T 300 500 8 10 1 1 0 0 1' + '\n', # Value
+                    'value=' + makevalue(resdict) + '\n'
+                ])
 
-""" main() """
-def main():
-    resdict = processconf(getconf())
-    resdict['name'] = makename(resdict)
-    makehorz(resdict)
-    makevert(resdict)
+
 
 if __name__ == "__main__":
     main()
